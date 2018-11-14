3918995d293accc42b184f265e086415ec06c168
Josephine Wright <jozright@gmail.com>
2018-11-14 14:14:25 +0100

    Update based on golint

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -2,12 +2,13 @@ package hamming
 
 import "errors"
 
+// Distance calculates Hamming edit distance
 func Distance(a, b string) (int, error) {
 	if len(b) != len(a) {
 		return -1, errors.New("strings 'a' and 'b' are unequal in length")
 	}
 	distance := 0
-	for i, _ := range a {
+	for i := range a {
 		if a[i] != b[i] {
 			distance++
 		}
