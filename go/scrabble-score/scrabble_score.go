04c58458a383ba5d85fc8f114d5f5eece97d5bef
Josephine Wright <jozright@gmail.com>
2018-11-16 12:42:06 +0100

    Use simpler map

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -4,24 +4,19 @@ import "strings"
 
 // Score words, scrabble style
 func Score(word string) int {
-	var scores = map[int][]string{
-		1:  []string{"a", "e", "i", "o", "u", "l", "n", "r", "s", "t"},
-		2:  []string{"d", "g"},
-		3:  []string{"b", "c", "m", "p"},
-		4:  []string{"f", "h", "v", "w", "y"},
-		5:  []string{"k"},
-		8:  []string{"j", "x"},
-		10: []string{"q", "z"},
+	var chars = map[string]int{
+		"a": 1, "e": 1, "i": 1, "o": 1, "u": 1, "l": 1, "n": 1, "r": 1, "s": 1, "t": 1,
+		"d": 2, "g": 2,
+		"b": 3, "c": 3, "m": 3, "p": 3,
+		"f": 4, "h": 4, "v": 4, "w": 4, "y": 4,
+		"k": 5,
+		"j": 8, "x": 8,
+		"q": 10, "z": 10,
 	}
 	finalScore := 0
 	for _, letter := range word {
-		for score := range scores {
-			for _, char := range scores[score] {
-				if strings.ToLower(string(letter)) == char {
-					finalScore = finalScore + score
-				}
-			}
-		}
+		score := chars[strings.ToLower(string(letter))]
+		finalScore = finalScore + score
 	}
 	return finalScore
 }
