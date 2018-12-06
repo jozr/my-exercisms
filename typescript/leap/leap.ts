988a6fe45e02bdf607c9b637a6291c53dbc03eca
Josephine Wright <jozright@gmail.com>
2018-12-06 17:55:52 +0100

    Pass leap

---
+++ typescript/leap/leap.ts
@@ -0,0 +1,8 @@
+function isLeapYear(year: number) {
+    if (year % 400 === 0 || year % 4 === 0 && year % 100 !== 0) {
+        return true;
+    }
+    return false;
+}
+
+export default isLeapYear
