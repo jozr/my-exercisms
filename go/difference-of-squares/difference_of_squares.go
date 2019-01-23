21808459976be13cfffd890161651cbe9abce45c
Josephine Wright <jozright@gmail.com>
2019-01-23 17:46:37 +0100

    Shorten link

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -8,7 +8,7 @@ func SquareOfSum(n int) int {
 
 // SumOfSquares sums the squares of all natural numbers up to n
 func SumOfSquares(n int) int {
-	// https://math.stackexchange.com/questions/183316/how-to-get-to-the-formula-for-the-sum-of-squares-of-first-n-numbers
+	// https://math.stackexchange.com/questions/183316/
 	return (n * (n + 1) * ((2 * n) + 1)) / 6
 }
 
